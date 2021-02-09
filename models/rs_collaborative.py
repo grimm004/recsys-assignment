1d13d5dd0bffbdbfadfe1007f923891f4eef012b
grimm004 <grimm004@gmail.com>
2021-02-09 14:58:30 +0000

    Added initial files.

---
+++ models/rs_collaborative.py
@@ -0,0 +1,11 @@
+#%%
+import sys
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import sklearn
+
+#%%
+
+# data = pd.read_json()
+
---
+++ models/rs_dataprep.py
@@ -0,0 +1,10 @@
+#%%
+from pathlib import Path
+
+#%%
+
+raw_data_path = Path().joinpath("data_raw", "yelp_academic_Dataset_business.json")
+
+with open(raw_data_path) as raw_data_file:
+    pass
+
